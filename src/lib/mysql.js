cb88c03448d2d0d97d951578c1dc1c9a489e65eb
Nefedyev Alexander <knighter@rambler.ru>
2022-08-08 11:48:00 +0900

    Fix mysql connection

--- src/lib/mysql.js
+++ src/lib/mysql.js
@@ -2,11 +2,12 @@
 
 const mysql = require('mysql2/promise');
 const { MYSLQ_ROOT_PASSWORD } = require('../../config');
+const host = process.env.DB_HOST || 'localhost';
 
 // Подключаемся к БД
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
+    host,
     user: 'root',
     password: MYSLQ_ROOT_PASSWORD,
     database: 'test_database'
